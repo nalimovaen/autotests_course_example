554389cf5047124e499456b25d62c71d266909fc
unknown <nalimova.en@gmail.com>
2024-10-16 15:07:26 +0700

    ветка для дз

---
+++ Homework3/homework/test_01.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+def plus(a, b):
+    return a + b
+
+
+def test_01():
+    assert plus(2, 2) == 4
+
+
+def test_02():
+    assert plus(100, 9) == 108
+
+@pytest.mark
+def test_03():
+    assert plus(-1, 1) == 0
+
+
+def test_zero():
+    assert plus(0, 0) == 0
+
